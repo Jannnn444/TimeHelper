d48b331e0c5c9c7a3686f2978d8cf572158816e9
Janus <Janus.Haung@hualiteq.com>
2025-08-19 16:38:19 +0800

    Initial Commit

---
+++ DailyTimeHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyTimeHelper
+//
+//  Created by Hualiteq International on 2025/8/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyTimeHelper/DailyTimeHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTimeHelperApp.swift
+//  DailyTimeHelper
+//
+//  Created by Hualiteq International on 2025/8/19.
+//
+
+import SwiftUI
+
+@main
+struct DailyTimeHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
